147e849aa1055b11997dd33e7199c7de29000595
M Harris <mallory@Ms-MacBook-Air.local>
2016-01-13 21:23:49 -0500

    two-dimensional graphics

--- src/one_dimension/core.clj
+++ src/one_dimension/core.clj
@@ -3,26 +3,32 @@
 (def G 1)
 (def M 1)
 (def m 1)
-(def h (bigdec 0.01 ))
+(def r 200)
+(def h (bigdec 40))
 
-(defn force [M m r] 
+(defn force [M m r p] 
   (let [r (double r)] 
-    (- (/ (* G M m) (* r r)))))
-(defn calc-accel [r] (/ (force M m r) m))
-(defn calc-position [r v] (+ r (* v h)))
+    (- (/ (* G M m p) (* r r r)))))
+(defn calc-accel [r p] (/ (force M m r p) m))
+(defn calc-position [p v] (+ p (* v h)))
 (defn calc-velo [v a] (+ v (* a h)))
-
+(defn initial-velo [M r] (Math/sqrt (/ (* G M) r)))
+  
 (defn check-state [{r :r}] 
   (pos? r))
 
 (defn update [state] 
-  (let [{r :r t :t v :v} state 
+  (let [{x :x y :y t :t vx :vx vy :vy} state 
          t (bigdec (+ t h) ) 
-         a (calc-accel r) 
-         r (calc-position r v) 
-         v (calc-velo v a)] 
-    {:r r :v v :t t}))
+         ax (calc-accel r x) 
+         ay (calc-accel r y ) 
+         vy (calc-velo vy ay) 
+         vx (calc-velo vx ax)
+         y (calc-position y vy) 
+         x (calc-position x vx)] 
+    {:x x :y y :vx vx :vy vy :t t}))
 
-(take-while check-state (iterate update {:r 1 :v 2 :t 1}))
-
-(take 5 (iterate update {:r 1 :v 2 :t 1}))
+;(take-while check-state (iterate update {:r 1 :v 2 :t 1}))
+;(def results (take 5 (iterate update {:x r :y 0 :vx 0 :vy (initial-velo M r) :t 0})) )
+;(def radii5 (map #(Math/sqrt (+ (* (% :x) (% :x)) (* (% :y) (% :y)))) results))
+;(/ (reduce + radii5) 5)
---
+++ src/one_dimension/graphics.clj
@@ -0,0 +1,44 @@
+(ns one-dimension.graphics
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [one-dimension.core :as g]))
+
+(defn setup []
+  ; Set frame rate to 30 frames per second.
+  (q/frame-rate 30)
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  ;(q/color-mode :hsb)
+  ; setup function returns initial state. It contains
+ {:x g/r :y 0 :vx 0 :vy (g/initial-velo g/M g/r) :t 0})
+
+(defn update-state [state]
+   (g/update state))
+
+(defn draw-state [state]
+  ; Clear the sketch by filling it with light-grey color.
+  (q/background 0)
+  ; Set star color.
+  (q/fill 255 255 0)
+  (let [x (state :x) 
+        y (state :y)]
+    ; Move origin point to the center of the sketch.
+    (q/ellipse (/ (q/width) 2) (/ (q/height) 2) 70 70)
+    (q/with-translation [(/ (q/width) 2)
+                         (/ (q/height) 2)]
+      ; Draw the planet.
+      (q/fill 0 0 255)
+      (q/ellipse x y 30 30))))
+
+(q/defsketch euler
+  :title "Orbiting Objects"
+  :size [500 500]
+  ; setup function called only once, during sketch initialization.
+  :setup setup
+  ; update-state is called on each iteration before draw-state.
+  :update g/update
+  :draw draw-state
+  :features [:keep-on-top]
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  :middleware [m/fun-mode])
